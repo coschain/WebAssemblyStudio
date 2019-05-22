d1faf29fb283e059b24969175143df7b6bcd9762
wangchao <wangchao4055@gmail.com>
2019-05-22 20:05:33 +0800

    fix multiple_cpp_project  build fail

--- templates/multiple_cpp_project/build.ts
+++ templates/multiple_cpp_project/build.ts
@@ -3,7 +3,7 @@ import { Service, project, ContractUtil } from "@wasm/studio-utils";
 
 // hello.cpp is target contract file (contract file should place in the first of array), in the following is dependent files
 gulp.task("build", async () => {
-  const data = await Service.compileFile([project.getFile("src/hello.cpp"),project.getFile("src/i18n.cpp")], "cpp", "wasm", "-g -O3");
+  const data = await Service.compileFile([project.getFile("src/hello.cpp"),project.getFile("src/i18n.cpp"),project.getFile("src/i18n.h")], "cpp", "wasm", "-g -O3");
   const outWasm = project.newFile("out/hello.wasm", "wasm", true);
   outWasm.setData(data);
   ContractUtil.addNewContractInfo("", data);
