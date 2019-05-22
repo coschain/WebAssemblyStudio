cfa0d71626466eacd7a6f15515e2b4fbdf675c1a
daishuo <daishuo@gmail.com>
2019-05-22 11:03:51 +0800

    try fix build script of multiple-cpp template

--- templates/multiple_cpp_project/abi.ts
+++ templates/multiple_cpp_project/abi.ts
@@ -3,7 +3,7 @@ import { Service, project, ContractUtil } from "@wasm/studio-utils";
 
 gulp.task("abi", async () => {
   // hello.cpp is target contract file (contract file should place in the first of array), in the following is dependent files
-  const data = await Service.compileAbi([project.getFile("src/hello.cpp"),project.getFile("src/i18n.hpp")], "cpp", "abi");
+  const data = await Service.compileAbi([project.getFile("src/hello.cpp"),project.getFile("src/i18n.cpp")], "cpp", "abi");
   const outAbi = project.newFile("out/hello.abi", "abi", true);
   outAbi.setData(data);
   ContractUtil.addNewContractInfo(data, "");
